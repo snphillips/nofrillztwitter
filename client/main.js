b3e24d5627a1d18dd28e3f25506bba367ab9a2d4
Sarah Phillips <snphillips@gmail.com>
2019-08-04 12:25:39 -0400

    client retreives data from server via axios, updates DOM

--- client/main.js
+++ client/main.js
@@ -1,19 +1,34 @@
-// https://www.sitepoint.com/single-page-app-without-framework/
+// const axios = require('axios')
 
 
 (function() {
 
+// this works
+// fetch(`http://localhost:3000/art-deco`)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+//     parseData(response)
+//     console.log("JSON.stringify(response)", JSON.stringify(response));
+//   })
+//   .then(function(response) {
+//   });
 
-fetch(`http://localhost:3000/art-deco`)
-  .then(function(response) {
-    return response.json();
+axios.get('http://localhost:3000/art-deco', {
+    params: {
+      ID: 12345
+    }
   })
-  .then(function(response) {
+  .then(function (response) {
+    console.log("console.log response:", response);
     parseData(response)
-    console.log("JSON.stringify(response)", JSON.stringify(response));
   })
-  .then(function(response) {
+  .catch(function (error) {
+    console.log(error);
   });
+
+
 
 
 
@@ -23,12 +38,12 @@ fetch(`http://localhost:3000/art-deco`)
    function parseData(response){
 
      console.log("hello from parseData")
-     console.log("TEST response.objects[0].title:", response.objects[0].title)
-     document.getElementById('test01').innerHTML = `title: ${response.objects[0].title} `
-     document.getElementById('test02').innerHTML = `year_acquired: ${response.objects[0].year_acquired} `
-     document.getElementById('test03').innerHTML = `decade: ${response.objects[0].decade} `
-     document.getElementById('test04').innerHTML = `medium: ${response.objects[0].medium} `
-     document.getElementById('test05').innerHTML = `description: ${response.objects[0].description} `
+     console.log("TEST response.objects[0].title:", response.data.objects[0].title)
+     document.getElementById('test01').innerHTML = `title: ${response.data.objects[0].title} `
+     document.getElementById('test02').innerHTML = `year_acquired: ${response.data.objects[0].year_acquired} `
+     document.getElementById('test03').innerHTML = `decade: ${response.data.objects[0].decade} `
+     document.getElementById('test04').innerHTML = `medium: ${response.data.objects[0].medium} `
+     document.getElementById('test05').innerHTML = `description: ${response.data.objects[0].description} `
 
    }
 
